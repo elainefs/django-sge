1fd4e88190df54c31b2cc836f14cd95562d9b634
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-06-24 00:29:28 -0300

    feat: add component to display information obtained from AI Agent

--- config/views.py
+++ config/views.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from ai.models import AIResult
 from config.metrics import (
     get_daily_sales_data,
     get_daily_sales_quantity_data,
@@ -21,6 +22,7 @@ def home(request):
     daily_sales_quantity = get_daily_sales_quantity_data()
     product_category_metric = get_product_category()
     product_brand_metric = get_product_brand()
+    ai_result = AIResult.objects.first()
 
     context = {
         "product_metrics": product_metrics,
@@ -29,6 +31,7 @@ def home(request):
         "daily_sales_quantity_data": json.dumps(daily_sales_quantity),
         "product_count_by_category": json.dumps(product_category_metric),
         "product_count_by_brand": json.dumps(product_brand_metric),
+        "ai_result": ai_result.result if ai_result else None,
     }
 
     return render(request, "home.html", context)
