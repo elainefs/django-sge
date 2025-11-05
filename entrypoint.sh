11a8898866e13f51a0670d435db64e66bd19c742
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-11-05 01:56:25 -0300

    fix: remove script for waiting PostgresSQL database create and update Run Server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Waiting for PostgreSQL..."
-until python -c "import psycopg2; psycopg2.connect(host='sge_db', port=5432, user='${POSTGRES_USER}', password='${POSTGRES_PASSWORD}', dbname='${POSTGRES_DB}')" 2>/dev/null; do
-  sleep 2
-done
-
 echo "Apply database migrations"
 python manage.py migrate
 
@@ -12,7 +7,10 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Load initial data"
-python manage.py loaddata config/fixtures/initial_user.json
+python manage.py loaddata config/fixtures/initial_user.json || true
 
 echo "Run Server"
-gunicorn --bind 0.0.0.0:$PORT --workers=4 config.wsgi:application --timeout 300
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:$PORT \
+  --workers 4 \
+  --timeout 300
