42065083b1eb34c234f6715f9e1cee0fc9c537d6
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-11-05 00:10:43 -0300

    feat: add favicon icon

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
 echo "Run Server"
 exec python manage.py runserver 0.0.0.0:8000
